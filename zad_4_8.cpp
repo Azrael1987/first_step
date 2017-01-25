c82f037c3a98acaa8bdbb66056bb2b60b6a0107d
Adam <gruszecki.adam@gmail.com>
2017-01-25 13:04:01 +0100

    Create zad_4_8.cpp

---
+++ zad_4_8.cpp
@@ -0,0 +1,39 @@
+// zad_4_8.cpp -- zadanie 8 zrodzial 4
+
+#include <string>
+#include <cstring>
+#include <iostream>
+
+struct pizza
+{
+	char name[20];
+	int size;
+	double weight;
+};
+
+int main()
+{
+	using namespace std;
+
+	pizza * pp = new pizza;
+
+	cout << "Podaj rozmiar pizzy: " << endl;
+	cin >> (*pp).size;
+	cout << "Podaj nazwe producenta pizzy: " << endl;
+	cin.get(pp->name, 20);				// tu program nie chce przyjac zmiennej
+	cout << "Podaj wage pizzy: " << endl;
+	cin >> (*pp).weight;
+
+	cout << "\n" << "Twoja pizza to: " << endl;
+	cout << "Rozmiar pizzy to: " << (*pp).size << endl;
+	cout << "Nazwa producenta: " << (*pp).name <<endl;
+	cout << "Waga pizzy to: " << (*pp).weight << endl;
+
+	delete pp;
+
+	cin.get();
+	cin.get();
+	cin.get();
+	cin.get();
+	return 0;
+}
