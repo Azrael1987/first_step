e7d97c53f667b7d87b9356207be9f3ccda7616c9
Adam <gruszecki.adam@gmail.com>
2017-02-22 18:23:21 +0100

    Update textin2.cpp

--- textin2.cpp
+++ textin2.cpp
@@ -1,32 +1,34 @@
-// textin2.cpp -- wczytywanie znakow  w petli while - do poprawy
-
+// textin2.cpp -- wczytywanie znakow  w petli while + petla opzniajaca wylaczenie
 #include<iostream>
+#include <ctime>	//zawiera deklarcje funkcji clock() i typu clock_t
 int main()
 {
 	using namespace std;
-	char ch;		// zmienna literowa
-	int count = 0;		// licznik wprowadzanych znakow
+	char ch;        // zmienna literowa
+	int count = 0;      // licznik wprowadzanych znakow
 
 	cout << "Podaj tekst do wprowadzenia; # konczy ciag znakow." << endl;
 	cin.get(ch);
-	
+
 	while (ch != '#')
 	{
-		
-		cout <<ch;		//wyswietl znak
-		++count;		//dolicz znak
-		
-		cin.get(ch);		//pobierz nastepny znak
-		
+
+		cout << ch;      //wyswietl znak
+		++count;        //dolicz znak
+		cin.get(ch);    //pobierz nastepny znak
+
 	}
 	cout << "\nWprowadziles " << count << " znakow" << endl;
-	 for (int i = 0; i <= count; i++)		//pochalnia entery ale nie wiem czemu nie wszystkie
-	{
-		cin.get();
-	}
 	cin.get();
-	cin.get();
-	cin.get();
-	cin.get();
+	cin.sync();
+
+	float secs = 5;
+	cin >> secs;
+	clock_t delay = secs * CLOCKS_PER_SEC;  //	konwersja na jednostyki systemowe
+	cout << "\nZaczynamy\a\n";
+	clock_t start = clock();
+	while (clock() - start < delay)
+		;
+	cout << "gotowe \a\n";
 	return 0;
 }
