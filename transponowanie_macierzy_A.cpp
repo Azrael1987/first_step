227e946d7cc953a68754c880bea5fb64d2d67213
Adam <gruszecki.adam@gmail.com>
2017-03-21 20:37:13 +0100

    Update transponowanie_macierzy_A.cpp

--- transponowanie_macierzy_A.cpp
+++ transponowanie_macierzy_A.cpp
@@ -1,11 +1,15 @@
-// transponowanie_macierzy.cpp -- transponowanie macierzy
+// transponowanie_macierzy.cpp -- transponowanie macierzy z zapisem do pliku (90)
 #include <iostream>
+#include<fstream>
 
 int main()
 {
 	using namespace std;
 	const int row = 3;				// ilosc wierszy w macierzy
 	const int col =4;				// ilosc kolumn w macierzy
+	
+	ofstream outFile;								// utworzenie obiektu na wyniki o nazwie outFile
+	outFile.open("macierz_transponowana.txt", ios::out | ios::app);		// powiazanie obiektu z plikiem, zapisuj | aktualizuj
 	
 	cout << "Program do transponowania macierzy." << endl << endl;
 
@@ -45,6 +49,32 @@ int main()
 		cout << endl;
 	};
 	cout << endl;
+
+	// zapisywanie wynikow do pliku tekstowego
+
+	outFile << "Twoja macierz A wyglada nastepujaca:" << endl << endl;		// zapisywanie macierz A
+	for (int r = 0; r < row; r++)
+	{
+		for (int c = 0; c < col; c++)
+		{
+			outFile << "\t" << A[r][c];
+		}
+		outFile << endl;
+	};
+	outFile << endl;
+
+	outFile << "Macierz transponowana B=A^T:" << endl << endl;	// zapisywanie macierz A
+	for (int r = 0; r < col; r++)     // ilosc wierszy w maciezy B jest przyrownywana do ilosci kolumn macierzy A
+	{
+		for (int c = 0; c < row; c++)	// ilosc kolumn w maciezy B jest przyrownywana do ilosci wierszy macierzy A
+		{
+			outFile << "\t" << B[r][c];
+		}
+		outFile << endl;
+	};
+	outFile << endl;
+
+	outFile.close();
 
 	cin.get();
 	return 0;
