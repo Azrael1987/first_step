21bf92e1351b0263f9fbdefd10d0ab0276bcaa98
Adam <gruszecki.adam@gmail.com>
2017-03-28 23:32:37 +0200

    Create zad_6_8.cpp

---
+++ zad_6_8.cpp
@@ -0,0 +1,71 @@
+// zad_6_8.cpp - zadanie 8 z rodzialu 6 - (124)
+
+#include <iostream>   // std::noskipws
+#include <fstream>    // ifstream
+#include <cstdlib>    // exit()
+#include <string>
+
+using namespace std;
+int main()
+{
+	const int Size = 60;
+	char filename[Size];
+	ifstream inFile;					// obiekt do wczytywania  z pliku
+
+	cout << "Podaj nazwe pliku: ";
+	cin.getline(filename, Size);		// nazwa pliku
+
+	inFile.open(filename);				// powiazanie obiektu z plikiem
+
+	if (!inFile.is_open())				// test otwarcia pliku
+	{
+		cout << "Otwracie pliku " <<filename<< " nie powiodlo sie.\n";
+		cout << "Program zaraz zostanei zamkniety.";
+		cin.get();
+		cin.get();
+		exit(EXIT_FAILURE);
+	}
+	else
+		cout << "Plik zostal otwarty.\n";
+		
+	char ch;
+	int count = 0;
+	
+	inFile >> noskipws >> ch;  //  noskipws -- nie pomijaj bialych znakow (z biblioteki <iostream>)
+
+	while (!inFile.eof())
+	{
+		if (inFile.good())
+		{
+			while (!inFile.eof())
+			{
+				count++;
+				inFile >> ch;
+			}
+		}
+		if (inFile.eof())
+		{
+			cout << "Koniec pliku\n";
+		}
+		else if (inFile.fail())
+		{
+			cout << "Wczytywanie danych zostalo przerwane. \n\aBlad typu pliku.";
+		}
+		else
+			cout << "Wczytywanie danych zostalo przerwane. \n\aNieznany blad.";
+		
+		inFile >> ch;
+	}
+
+	if(count == 0)
+	{
+		cout << "Brak danych do wczytania.\nPlik pusty.";
+	}
+	else
+	{
+		cout << "\nPlik " << filename << " zawiera " << count << " znakow (w tym spacje i znaki nowej linii).\n";
+	}
+		inFile.close();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		return 0;
+}
