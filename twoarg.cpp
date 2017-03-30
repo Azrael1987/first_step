0b2cd5981dec3e069733727289b94e685a11433f
Adam <gruszecki.adam@gmail.com>
2017-03-30 14:35:08 +0200

    Update twoarg.cpp

--- twoarg.cpp
+++ twoarg.cpp
@@ -4,12 +4,12 @@
 
 using namespace std;
 
-void n_char(char , int  );			// prototyp funkcji
+void n_chars(char , int  );			// prototyp funkcji
 
 int main()
 {
 	char ch;
-	int time;
+	int times;
 
 	cout << "Podaj znak: ";
 	cin >> ch;
@@ -17,12 +17,12 @@ int main()
 	while (ch != 'q')
 	{
 		cout << "Podaj liczbe calkowita: ";
-		cin >> time;
-		n_char(ch, time);			// wywolanie funkcji 2-argumentowej
+		cin >> times;
+		n_chars(ch, times);			// wywolanie funkcji 2-argumentowej
 		cout << "\nPodaj nastepny znak; liter q konczy program: ";
 		cin >> ch;
 	}
-	cout << "Wartosc zmiennej time to " << time << ".\n";
+	cout << "Wartosc zmiennej time to " << times << ".\n";
 	cout << "Do widzenia.";
 	
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -30,7 +30,7 @@ int main()
 	return 0;
 }
 
-void n_char(char c, int n)			// deklaracja funkcji - wyswietl znak c n razy
+void n_chars(char c, int n)			// deklaracja funkcji - wyswietl znak c n razy
 {
 	while (n-- > 0)					// dopoki n nie osiagnie wartosci 0
 	{
